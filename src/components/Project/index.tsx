7e64fac2b7af6a3fed2dce918a427043e43d4363
Hitalo <hitalo.2014.hs@gmail.com>
2022-08-14 17:44:46 -0300

    fix: not identified

--- src/components/Project/index.tsx
+++ src/components/Project/index.tsx
@@ -62,7 +62,7 @@ export const Project = (): JSX.Element => {
             ) : (
               <ProjectStackTech>
                 <Text color="grey2" type="body2">
-                  Not identified
+                  Javascript
                 </Text>
               </ProjectStackTech>
             )}
